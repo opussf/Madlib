352ea39e7dc149b5d863f1474f175d6e4c171c55
opussf <github@zz9-za.com>
2024-04-19 01:53:08 -0700

    A good start

--- src/Madlib.lua
+++ src/Madlib.lua
@@ -16,4 +16,81 @@ COLOR_GOLD = "|cffcfb52b"
 COLOR_NEON_BLUE = "|cff4d4dff"
 COLOR_END = "|r"
 
-MADLIB_Data = {}
+MADLIB_Data = {
+	{ 	["story"] = "It was a %s, cold November day. I woke up to the %s smell of %s roasting in the %s downstairs. ",
+		["terms"] = { "Adjective", "Adjective", "Type of Bird", "Room in a House" }
+	}
+}
+
+function MADLIB.OnLoad()
+	StripDiceFrame:RegisterEvent( "CHAT_MSG_GUILD" )
+end
+function MADLIB.OnUpdate()
+	if MADLIB.game then  -- only need to do something here
+		if not MADLIB.game.voteTerms.votes and time() > MADLIB.game.voteTerms.started+30 then
+			MADLIB.VoteForTerms()
+		end
+	end
+end
+
+function MADLIB.Print( msg )
+	SendChatMessage( msg, "GUILD", nil, nil )
+end
+
+function MADLIB.StartGame( param )
+	param = tonumber( param )
+	if MADLIB.game then
+		MADLIB.Print( "A game has already started. Please respond to the current prompt." )
+	else
+		MADLIB.game = {
+				["index"] = (param and param <= #MADLIB_Data) and param or random(#MADLIB_Data),
+				["terms"] = {},
+				["started"] = time()}
+		MADLIB.Print( "Hello, welcome to Guild Madlibs. Please start your responses with \"ml: \" (no quotes). Thanks." )
+		MADLIB.AskForTerm()
+	end
+end
+
+function MADLIB.AskForTerm()
+	termIndex = #MADLIB.game.terms + 1
+	termType = MADLIB_Data[MADLIB.game.index].terms[termIndex]
+	termPre = string.find( termType, "^[aAeEiIoOuU]" ) and "an" or "a"
+	MADLIB.game.voteTerms = { ["started"] = time(), ["terms"] = {} }
+	MADLIB.Print( string.format( "Please give me %s %s. You have 30 seconds to submit an answer.", termPre, termType ) )
+end
+function MADLIB.GetTerm( term )
+	if MADLIB.game.voteTerms then
+		MADLIB.game.voteTerms.terms[term] = true
+	end
+end
+function MADLIB.VoteForTerms()
+	MADLIB.game.voteTerms.votes = {}
+
+	-- I have recieved 3 suggestions for Noun.  Please vote for your favorite: 1 - chair, 2 - desk, 3 - floor.
+end
+
+MADLIB.commandList = {
+	["start"] = { ["func"] = MADLIB.StartGame },
+}
+
+function MADLIB.CHAT_MSG_GUILD(...)
+	_, msg, player, language, _, _, other = ...
+	-- print( msg )
+	s, e, cmd, param = string.find( string.lower( msg ), "^ml: ([^ ]+) *([^ ]*)" )
+	print( s, e, cmd, param )
+	if s then
+		if MADLIB.commandList[cmd] and MADLIB.commandList[cmd].func then
+			MADLIB.commandList[cmd].func( param )
+		else
+			MADLIB.GetTerm( cmd )
+		end
+	end
+
+
+
+
+	--SendChatMessage( STEPS.GetPostString(), chatChannel, nil, toWhom )
+
+end
+
+-- string.format( "%s %s", table.unpack( t ) )
--- test/test.lua
+++ test/test.lua
@@ -9,8 +9,31 @@ test.outFileName = "testOut.xml"
 ParseTOC( "../src/Madlib.toc" )
 
 function test.before()
+	MADLIB.game = nil
+	chatLog = {}
 end
 function test.after()
+end
+function test.test_start_lower()
+	MADLIB.CHAT_MSG_GUILD( "", "ml: start", "user1" )
+	assertTrue( MADLIB.game, "game table should be created." )
+	assertEquals( 1, MADLIB.game.index, "game index should be 1." )
+	assertTrue( MADLIB.game.terms, "game terms should be created." )
+	assertEquals( time(), MADLIB.game.started, "game start should be set.")
+end
+function test.test_start_upper()
+	MADLIB.CHAT_MSG_GUILD( "", "ML: START", "user1" )
+	assertTrue( MADLIB.game.terms, "game terms should be created." )
+end
+function test.test_start_specific()
+	MADLIB.CHAT_MSG_GUILD( "", "ML: START 1", "user1" )
+	assertTrue( MADLIB.game.terms, "game terms should be created." )
+end
+function test.test_give_adjective_1()
+	MADLIB.CHAT_MSG_GUILD( "", "ML: START 1", "user1" )
+	MADLIB.CHAT_MSG_GUILD( "", "ML: broken", "user1" )
+	assertTrue( MADLIB.game.voteTerms.terms, "game terms should be created." )
+	assertTrue( MADLIB.game.voteTerms.terms["broken"] )
 end
 
 
