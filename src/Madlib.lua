d5a098140983edbf6c6a867ec268fcb0fe5b26dd
opussf <github@zz9-za.com>
2024-04-19 14:12:19 -0700

    Work on Voting

--- src/Madlib.lua
+++ src/Madlib.lua
@@ -23,12 +23,15 @@ MADLIB_Data = {
 }
 
 function MADLIB.OnLoad()
-	StripDiceFrame:RegisterEvent( "CHAT_MSG_GUILD" )
+	MadlibFrame:RegisterEvent( "CHAT_MSG_GUILD" )
 end
 function MADLIB.OnUpdate()
 	if MADLIB.game then  -- only need to do something here
-		if not MADLIB.game.voteTerms.votes and time() > MADLIB.game.voteTerms.started+30 then
+		if not MADLIB.game.voteTerms.map and time() > MADLIB.game.voteTerms.started+30 then
 			MADLIB.VoteForTerms()
+		end
+		if MADLIB.game.voteTerms.closeAt and time() > MADLIB.game.voteTerms.closeAt then
+			MADLIB.ResolveVotes()
 		end
 	end
 end
@@ -45,8 +48,9 @@ function MADLIB.StartGame( param )
 		MADLIB.game = {
 				["index"] = (param and param <= #MADLIB_Data) and param or random(#MADLIB_Data),
 				["terms"] = {},
-				["started"] = time()}
-		MADLIB.Print( "Hello, welcome to Guild Madlibs. Please start your responses with \"ml: \" (no quotes). Thanks." )
+				["started"] = time()
+		}
+		MADLIB.Print( "Hello, welcome to Guild Madlibs. Please start your responses with \"ml: \" (no quotes)." )
 		MADLIB.AskForTerm()
 	end
 end
@@ -59,15 +63,25 @@ function MADLIB.AskForTerm()
 	MADLIB.Print( string.format( "Please give me %s %s. You have 30 seconds to submit an answer.", termPre, termType ) )
 end
 function MADLIB.GetSubmission( term )
+	print( "GetSubmission( "..term.." )" )
 	-- really generic, context inputs
 	if MADLIB.game.voteTerms then
-		MADLIB.game.voteTerms.terms[term] = true
+		print("I have voteTerms")
+		if MADLIB.game.voteTerms.map then
+			print("I have voteTerms.map")
+			voteVal = tonumber( term )
+			print( "voteVal: "..voteVal )
+			if voteVal then
+				MADLIB.game.voteTerms.terms[ MADLIB.game.voteTerms.map[voteVal] ] =
+						MADLIB.game.voteTerms.terms[ MADLIB.game.voteTerms.map[voteVal] ] + 1
+			end
+		else
+			MADLIB.game.voteTerms.terms[term] = true
+		end
 	end
 end
 function MADLIB.VoteForTerms()
 	print( "VoteForTerms" )
-	-- MADLIB.game.voteTerms.votes = {}
-	-- MADLIB.game.voteTerms.count = 0
 	MADLIB.game.voteTerms.map = {}
 	for term, _ in pairs( MADLIB.game.voteTerms.terms ) do
 		print( term )
@@ -75,12 +89,24 @@ function MADLIB.VoteForTerms()
 		MADLIB.game.voteTerms.terms[term] = 0
 	end
 	if #MADLIB.game.voteTerms.map == 1 then
-		table.insert( MADLIB.game.terms, MADLIB.game.voteTerms.map[1] )
-	-- else
+		MADLIB.game.voteTerms.closeAt = time()
+	elseif #MADLIB.game.voteTerms.map > 1 then
+		MADLIB.game.voteTerms.voteStrTable = {}
+		for i, t in ipairs( MADLIB.game.voteTerms.map ) do
+			table.insert( MADLIB.game.voteTerms.voteStrTable, i.." - "..t )
+		end
+		MADLIB.Print( string.format( "I have recieved %d suggestions for %s. You have 30 seconds to vote for your favorite:",
+				#MADLIB.game.voteTerms.map,
+				MADLIB_Data[MADLIB.game.index].terms[#MADLIB.game.terms + 1]
+		) )
+		MADLIB.Print( table.concat( MADLIB.game.voteTerms.voteStrTable, ", " ) )
+		MADLIB.game.voteTerms.closeAt = time() + 30
+		-- table.insert( MADLIB.game.terms, MADLIB.game.voteTerms.map[1] )
+	-- else  -- what if there are no terms to vote on.  end game?
 	end
+end
+function MADLIB.ResolveVotes()
 
-
-	-- I have recieved 3 suggestions for Noun.  Please vote for your favorite: 1 - chair, 2 - desk, 3 - floor.
 end
 
 MADLIB.commandList = {
--- test/test.lua
+++ test/test.lua
@@ -48,10 +48,65 @@ function test.test_1_term_timeOut_onupdate()
 		}
 	}
 	MADLIB.OnUpdate()
+	assertEquals( time(), MADLIB.game.voteTerms.closeAt )
 	assertEquals( "broken", MADLIB.game.terms[1], "broken should be added to list." )
 end
+function test.test_2_terms_timeOut_onupdate_voteStarted()
+	MADLIB.game = {
+		["index"] = 1,
+		["terms"] = {},
+		["started"] = time() - 31,
+		["voteTerms"] = {
+			["started"] = time() - 31,
+			["terms"] = {
+				["broken"] = true,
+				["red"] = true,
+			},
+		}
+	}
+	MADLIB.OnUpdate()
+	voteVal = 0
+	for i, t in ipairs( MADLIB.game.voteTerms.map ) do
+		if t == "red" then
+			voteVal = i
+		end
+	end
+	MADLIB.CHAT_MSG_GUILD( "", "ml: "..voteVal, "user1" )
+	assertEquals( 1, MADLIB.game.voteTerms.terms.red, "red should have 1 vote." )
+	assertEquals( 0, MADLIB.game.voteTerms.terms.broken, "broken should have 0 votes." )
+end
+function test.test_2_terms_timeOut_onupdate_voteStarted()
+	MADLIB.game = {
+		["index"] = 1,
+		["terms"] = {},
+		["started"] = time() - 31,
+		["voteTerms"] = {
+			["started"] = time() - 31,
+			["closeAt"] = time(),
+			["map"] = { "broken", "red", "persistent" },
+			["terms"] = {
+				["broken"] = 0,
+				["red"] = 0,
+				["persistent"] = 1,
+			},
+		},
+	}
+	MADLIB.OnUpdate()
+	-- assertEquals( 1, MADLIB.game.voteTerms.terms.red, "red should have 1 vote." )
+	-- assertEquals( 0, MADLIB.game.voteTerms.terms.broken, "broken should have 0 votes." )
+end
 
-
+function test.notest_fullGame()
+	-- Start game
+	MADLIB.CHAT_MSG_GUILD( "", "ml: start", "user1" )
+	for _, s in ipairs( chatLog ) do
+		print( s.msg )
+	end
+	MADLIB.CHAT_MSG_GUILD( "", "ml: persistent", "user1" )
+	for _, s in ipairs( chatLog ) do
+		print( s.msg )
+	end
+end
 
 
 test.run()
