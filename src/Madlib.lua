b04722ed4a7810670b97a6dec659d6d6afe503f9
opussf <github@zz9-za.com>
2024-04-18 22:08:13 -0700

    Initial checkin

---
+++ src/Madlib.lua
@@ -0,0 +1,19 @@
+-- MADLIB @VERSION@
+MADLIB_SLUG, MADLIB = ...
+MADLIB_MSG_ADDONNAME = GetAddOnMetadata( MADLIB_SLUG, "Title" )
+MADLIB_MSG_VERSION   = GetAddOnMetadata( MADLIB_SLUG, "Version" )
+MADLIB_MSG_AUTHOR    = GetAddOnMetadata( MADLIB_SLUG, "Author" )
+
+-- Colours
+COLOR_RED = "|cffff0000"
+COLOR_GREEN = "|cff00ff00"
+COLOR_BLUE = "|cff0000ff"
+COLOR_PURPLE = "|cff700090"
+COLOR_YELLOW = "|cffffff00"
+COLOR_ORANGE = "|cffff6d00"
+COLOR_GREY = "|cff808080"
+COLOR_GOLD = "|cffcfb52b"
+COLOR_NEON_BLUE = "|cff4d4dff"
+COLOR_END = "|r"
+
+MADLIB_Data = {}
